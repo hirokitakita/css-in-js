2128c4d38ce58af0b22f5baac9a77431f22603d6
hirokitakita <96803485+hirokitakita@users.noreply.github.com>
2022-01-01 13:51:08 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { CssModules } from "./components/CssModules";
+import { InlineStyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
+import { Emotion } from "./components/Emotion";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
+    </div>
+  );
+};
